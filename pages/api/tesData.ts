250ecafbf8b803132fd210c3ef74a525ac3097b0
Muhammad  Adil <adilmuhammad706@gmail.com>
2022-12-08 16:06:33 +0700

    Update

---
+++ pages/api/tesData.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    "id": '1',
+    "about": "TEST ID 3"
+  },
+  {
+    "id": '2',
+    "about": "TEST ID 2"
+  }
+]
---
+++ pages/dir/test.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+function Hello() {
+    return (
+        <Link href={'/'}><h1>HALO AKHIRAT</h1></Link>
+    )
+}
+
+export default Hello
